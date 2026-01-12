c5bae7ef304e25b17871808d8f7bcf5a8f2e56db
Alexander <alexv2995@gmail.com>
2026-01-12 02:25:30 -0500

    mensaje del commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+# Guía de Instalación
+
+## Requisitos
+- Git 2.40 o superior
+- Cuenta en GitHub
+- GPG configurado para commits firmados
+
+## Clonar el repositorio
+```bash
+git clone git@github.com:alexv2995-alt/scv-empresarial.git
+cd scv-empresarial
